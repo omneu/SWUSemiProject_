c33c97ea56db268454ec8319e264e0d7ce194f6f
omneu <heaju_724@naver.com>
2019-07-03 16:54:20 +0900

    강아지 사진을 추가함

--- app/src/main/java/com/swu/swusemiproject_/MainActivity.java
+++ app/src/main/java/com/swu/swusemiproject_/MainActivity.java
@@ -2,7 +2,10 @@ package com.swu.swusemiproject_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //태스트 코드1
-
-        //오늘 컨디션 중간
-
-        //커밋 및 push 테스트
     }
+
+
 }
---
+++ app/src/main/java/com/swu/swusemiproject_/register.java
@@ -0,0 +1,14 @@
+package com.swu.swusemiproject_;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class register extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
