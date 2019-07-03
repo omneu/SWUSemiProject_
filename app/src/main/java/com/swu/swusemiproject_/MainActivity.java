6bed7b4e1801ea031b7e52486e6a356e761c837e
omneu <heaju_724@naver.com>
2019-07-03 15:01:15 +0900

    강아지 사진을 추가함

--- app/src/main/java/com/swu/swusemiproject_/MainActivity.java
+++ app/src/main/java/com/swu/swusemiproject_/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //태스트 코드1
-        
+
         //오늘 컨디션 중간
 
         //커밋 및 push 테스트
